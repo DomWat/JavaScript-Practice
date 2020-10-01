df91c7b7f1e5027b46c80762b7c2ca4ba1327b80
DomWat <domwat20@gmail.com>
2020-10-01 13:32:55 -0500

    news api

---
+++ news-app.js
@@ -0,0 +1,15 @@
+let post = document.getElementById("post")
+
+const newsItems = news.articles.map((article) => {
+    return `
+    <li>
+    <h1><u><b>${article.title}</b></u></h1>
+    <br><label>${article.author}</label>
+    <br><p>${article.description}</p>
+    <br><a href =${article.url}>Read Here</a>
+    <br><img src =${article.urlToImage} height = 200px width = 150px/>
+    <br><label>Published at: ${article.publishedAt}</label>
+    </li>
+    `
+})
+post.insertAdjacentHTML('beforeend',newsItems.join(''))
