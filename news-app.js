698bd60c6bbdf315618bf203ec2055c47dcf45fd
DomWat <domwat20@gmail.com>
2020-10-01 15:13:51 -0500

    add alt to image

--- news-app.js
+++ news-app.js
@@ -7,7 +7,7 @@ const newsItems = news.articles.map((article) => {
     <br><label>${article.author}</label>
     <br><p>${article.description}</p>
     <br><a href =${article.url}>Read Here</a>
-    <br><img src =${article.urlToImage} height = 200px width = 150px/>
+    <br><img src =${article.urlToImage} height = 200px width = 150px alt = "picture"/>
     <br><label>Published at: ${article.publishedAt}</label>
     </li>
     `
